84c986f12b735a6360860760f1092c53f84814d4
Devansu <devansuyadav@gmail.com>
2022-08-12 17:58:26 +0530

    feat: mongodb and express app setup

---
+++ db/db.connect.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connectDB = () => {
+    try {
+        mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log("Connected to Database successfully");
+    } catch (err) {
+        console.log("Failed to connect to DB");
+    }
+};
+
+module.exports = {
+    connectDB
+};
